443e9b8e859058835cdead3f98fdeda4e28d5804
mi2428 <mi2428782020@gmail.com>
2019-06-21 17:02:13 +0900

    Bugfix: skip when relurl is dead
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- script/nc2xxxxx.web.fc2.com/illusts.py
+++ script/nc2xxxxx.web.fc2.com/illusts.py
@@ -13,7 +13,8 @@ driver = webdriver.Chrome(chrome_options=options)
 ## http://nc2xxxxx.web.fc2.com/
 baseurl = 'http://nc2xxxxx.web.fc2.com/stock/'
 subpages = []
-for i in range(57):
+start = 11
+for i in range(start, 57):
     n = str(i*10).zfill(3)
     subpages.append("{}.html".format(n))
 
@@ -33,8 +34,8 @@ def routine(pageurl):
     page_height = driver.execute_script('return document.body.scrollHeight')
     driver.set_window_size(page_width, page_height)
     screenshot_name = "screenshot_{}.png".format(pageurl.split('/')[-1])
-    driver.save_screenshot(screenshot_name)
     print(pageurl)
+    driver.save_screenshot(screenshot_name)
 
     ## 2. Download all images
     image_url_lst = []
@@ -42,13 +43,18 @@ def routine(pageurl):
     rel_url_lst.extend([tag.get_attribute('src') for tag in driver.find_elements_by_tag_name('img')])
     rel_url_lst.extend([tag.get_attribute('href') for tag in driver.find_elements_by_tag_name('a')])
     for relurl in rel_url_lst:
+        if relurl is None:
+            continue
         ext = relurl.split('/')[-1].split('.')[-1]
         if ext.lower() in image_extensions:
             image_url_lst.append(urljoin(baseurl, relurl))
     for img_url in image_url_lst:
         image_name = img_url.split('/')[-1]
-        request.urlretrieve(img_url, image_name)
         print(img_url)
+        try:
+            request.urlretrieve(img_url, image_name)
+        except Exception:
+            print("skipped:", img_url)
 
 
 if __name__ == '__main__':
